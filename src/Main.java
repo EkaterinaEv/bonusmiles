92c0139db9a5e683d63ad829a1fa574aa1a8110e
Ekaterina Evtyukhova <katyev60@gmail.com>
2023-07-07 12:24:48 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketprice = 13676; // стоимость билета
-        int bonusprice = 20; // количество рублей для одной бонусной мили
+        int ticketPrice = 13676; // стоимость билета
+        int bonusPrice = 20; // количество рублей для одной бонусной мили
 
         System.out.println("Количество начисленных миль за купленный билет:");
-        System.out.println(ticketprice / bonusprice);
+        System.out.println(ticketPrice / bonusPrice);
     }
 }
